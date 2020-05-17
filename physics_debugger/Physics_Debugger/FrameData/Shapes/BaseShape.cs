33b00a7968c1588ef6e1c217f51b4228320e2e9b
RythBlade <redeyescf@hotmail.com>
2020-05-17 17:05:31 +0100

    Added an implementation of convex hull data and a base shape for all shapes to inherit from.

---
+++ physics_debugger/Physics_Debugger/FrameData/Shapes/BaseShape.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace physics_debugger.FrameData.Shapes
+{
+    public enum ShapeType : uint
+    {
+        eObb
+        , eSphere
+        , eCone
+        , eConvexHull
+    };
+
+    public class BaseShape
+    {
+        public int Id = -1;
+        public bool HasLocalMatrix = false;
+        public Matrix4x4 LocalMatrix = new Matrix4x4();
+        public ShapeType ShapeType = ShapeType.eObb;
+    }
+}
---
+++ physics_debugger/Physics_Debugger/FrameData/Shapes/ConvexHull.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Numerics;
+
+namespace physics_debugger.FrameData.Shapes
+{
+    public class ConvexHull : BaseShape
+    {
+        public class Face
+        {
+            public int[] Index = new int[] { 0, 0, 0 };
+        }
+
+        public class Vertex
+        {
+            public Vector4 Point = new Vector4();
+            public Vector4 Normal = new Vector4();
+        }
+
+        public List<Vertex> Vertices = new List<Vertex>();
+        public List<Face> Faces = new List<Face>();
+    }
+}
