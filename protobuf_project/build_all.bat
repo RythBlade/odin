33b6a50c67909848faf3ed61f9bc7046a292d1a7
RythBlade <redeyescf@hotmail.com>
2020-05-26 20:54:53 +0100

    Renamed particle.proto to rigid_body.proto and copied the generated files to the projects

--- protobuf_project/build_all.bat
+++ protobuf_project/build_all.bat
@@ -1,3 +1,3 @@
-protobufCompiler\protoc.exe --proto_path="%cd%\proto" --proto_path="%cd%\protobufCompiler" --cpp_out="%cd%\generated\cpp\include" --csharp_out="%cd%\generated\csharp" "%cd%\proto\particle.proto"
+protobufCompiler\protoc.exe --proto_path="%cd%\proto" --proto_path="%cd%\protobufCompiler" --cpp_out="%cd%\generated\cpp\include" --csharp_out="%cd%\generated\csharp" "%cd%\proto\rigid_body.proto"
 
 protobufCompiler\protoc.exe --proto_path="%cd%\proto" --proto_path="%cd%\protobufCompiler" --cpp_out="%cd%\generated\cpp\include" --csharp_out="%cd%\generated\csharp" "%cd%\proto\base_types.proto"
