94a3e70bef292e70bedcea6b45058ae1fd415323
RythBlade <redeyescf@hotmail.com>
2020-05-28 21:44:12 +0100

    Cleaned up the old shape created sync functionality as the new protobuf version has it covered

--- physics_debugger/Telemetry/Network/PacketTranslator.cs
+++ physics_debugger/Telemetry/Network/PacketTranslator.cs
@@ -86,99 +86,6 @@ namespace Telemetry.Network
             }
         }
 
-        /*private void ProcessShapeAdded(BasePacketHeader1 packet)
-        {
-            // -- shape packet layout --
-            //      int         shape ID
-            //      uint        shape type
-            //      uint        has local matrix
-            //      float[16]   local matrix
-            //      -- Obb -- 
-            //          float[3]    half extents
-            //      
-            //      -- Convex hull --
-            //          uint                number of vertices
-            //          uint                number of faces
-            //          float[3]float[3]    vertices
-            //          int[3]              faces
-
-            BaseShape baseShape = null;
-
-            int byteIndex = BasePacketHeader1.StartOfPacketData;
-
-            int shapeId = BitConverter.ToInt32(packet.PacketBytes, byteIndex); byteIndex += 4;
-            uint shapeType = BitConverter.ToUInt32(packet.PacketBytes, byteIndex); byteIndex += 4;
-            uint hasLocalMatrix = BitConverter.ToUInt32(packet.PacketBytes, byteIndex); byteIndex += 4;
-
-            System.Numerics.Matrix4x4 localMatrix = new System.Numerics.Matrix4x4();
-
-            localMatrix.M11 = BitConverter.ToSingle(packet.PacketBytes, byteIndex); byteIndex += 4;
-            localMatrix.M12 = BitConverter.ToSingle(packet.PacketBytes, byteIndex); byteIndex += 4;
-            localMatrix.M13 = BitConverter.ToSingle(packet.PacketBytes, byteIndex); byteIndex += 4;
-            localMatrix.M14 = BitConverter.ToSingle(packet.PacketBytes, byteIndex); byteIndex += 4;
-            localMatrix.M21 = BitConverter.ToSingle(packet.PacketBytes, byteIndex); byteIndex += 4;
-            localMatrix.M22 = BitConverter.ToSingle(packet.PacketBytes, byteIndex); byteIndex += 4;
-            localMatrix.M23 = BitConverter.ToSingle(packet.PacketBytes, byteIndex); byteIndex += 4;
-            localMatrix.M24 = BitConverter.ToSingle(packet.PacketBytes, byteIndex); byteIndex += 4;
-            localMatrix.M31 = BitConverter.ToSingle(packet.PacketBytes, byteIndex); byteIndex += 4;
-            localMatrix.M32 = BitConverter.ToSingle(packet.PacketBytes, byteIndex); byteIndex += 4;
-            localMatrix.M33 = BitConverter.ToSingle(packet.PacketBytes, byteIndex); byteIndex += 4;
-            localMatrix.M34 = BitConverter.ToSingle(packet.PacketBytes, byteIndex); byteIndex += 4;
-            localMatrix.M41 = BitConverter.ToSingle(packet.PacketBytes, byteIndex); byteIndex += 4;
-            localMatrix.M42 = BitConverter.ToSingle(packet.PacketBytes, byteIndex); byteIndex += 4;
-            localMatrix.M43 = BitConverter.ToSingle(packet.PacketBytes, byteIndex); byteIndex += 4;
-            localMatrix.M44 = BitConverter.ToSingle(packet.PacketBytes, byteIndex); byteIndex += 4;
-
-            switch(shapeType)
-            {
-                case (uint)ShapeType.eObb:
-                    break;
-
-                case (uint)ShapeType.eConvexHull:
-                    ConvexHull convexHull = new ConvexHull();
-                    baseShape = convexHull;
-
-                    convexHull.Id = shapeId;
-                    convexHull.HasLocalMatrix = hasLocalMatrix != 0;
-                    convexHull.LocalMatrix = localMatrix;
-                    convexHull.ShapeType = ShapeType.eConvexHull;
-
-                    uint numberOfVertices = BitConverter.ToUInt32(packet.PacketBytes, byteIndex); byteIndex += 4;
-                    uint numberOfFaces = BitConverter.ToUInt32(packet.PacketBytes, byteIndex); byteIndex += 4;
-
-                    for(uint i = 0; i < numberOfVertices; ++i)
-                    {
-                        ConvexHull.Vertex newVertex = new ConvexHull.Vertex();
-
-                        newVertex.Point.X = BitConverter.ToSingle(packet.PacketBytes, byteIndex); byteIndex += 4;
-                        newVertex.Point.Y = BitConverter.ToSingle(packet.PacketBytes, byteIndex); byteIndex += 4;
-                        newVertex.Point.Z = BitConverter.ToSingle(packet.PacketBytes, byteIndex); byteIndex += 4;
-                        newVertex.Point.W = BitConverter.ToSingle(packet.PacketBytes, byteIndex); byteIndex += 4;
-
-                        newVertex.Normal.X = BitConverter.ToSingle(packet.PacketBytes, byteIndex); byteIndex += 4;
-                        newVertex.Normal.Y = BitConverter.ToSingle(packet.PacketBytes, byteIndex); byteIndex += 4;
-                        newVertex.Normal.Z = BitConverter.ToSingle(packet.PacketBytes, byteIndex); byteIndex += 4;
-                        newVertex.Normal.W = BitConverter.ToSingle(packet.PacketBytes, byteIndex); byteIndex += 4;
-
-                        convexHull.Vertices.Add(newVertex);
-                    }
-
-                    for (uint i = 0; i < numberOfFaces; ++i)
-                    {
-                        ConvexHull.Face newFace = new ConvexHull.Face();
-
-                        newFace.Index[0] = BitConverter.ToInt32(packet.PacketBytes, byteIndex); byteIndex += 4;
-                        newFace.Index[1] = BitConverter.ToInt32(packet.PacketBytes, byteIndex); byteIndex += 4;
-                        newFace.Index[2] = BitConverter.ToInt32(packet.PacketBytes, byteIndex); byteIndex += 4;
-
-                        convexHull.Faces.Add(newFace);
-                    }
-                    break;
-            }
-
-            AddedShapes.Add(baseShape.Id, baseShape);
-        }*/
-
         public bool TranslatePacket(BasePacketHeader packet)
         {
             bool toReturn = false;
@@ -201,23 +108,6 @@ namespace Telemetry.Network
                     default:
                         break;
                 }
-
-                /*
-                switch(packet.messageHeader.MessageType)
-                {
-                    case (uint)PacketType.eRigidBodyFrameUpdate:
-                        ProcessRigidBodyFrameUpdate(packet, snapshot);
-                        toReturn = true;
-                        break;
-
-                    case (uint)PacketType.eShapeAdded:
-                        ProcessShapeAdded(packet);
-                        toReturn = true;
-                        break;
-
-                    default:
-                        break;
-                }*/
             }
 
             return toReturn;
